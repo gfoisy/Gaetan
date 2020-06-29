ee3707acba9167e75b693603f4a1442b1a2a084e
gfoisy <gfoisy@u.rochester.edu>
2020-06-29 19:33:54 -0400

    extruder moves!
    extruder moves, now it needs to slowly reveal the object in addition to moving smoother.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,21 @@
-const extruder= document.getElementById('extruder');
-let steps=-65;
+const extruder_left= document.getElementById('extruder-left');
+const extruder_right= document.getElementById('extruder-right');
+const extruder_bottom_editor=document.getElementById('extruder');
+let steps=0;
+let negpos=1;
+let height=7.5;
 
 window.onwheel=function printGo(){
-extruder.style= 'left: '+steps+'rem;';
-steps=steps+1;
+  extruder_right.style= 'left: '+ -steps+'%;';
+  extruder_left.style= 'right: '+steps+'%;';
+  steps=steps+5*negpos;
+  if(steps>25){
+    negpos=-1;
+    height=height+5;
+    extruder_bottom_editor.style= 'bottom: '+ height+'%;';
+  }else if(steps<-25){
+    negpos=1;
+    height=height+5;
+    extruder_bottom_editor.style= 'bottom: '+ height+'%;';
+    }
 };
