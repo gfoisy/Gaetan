f3aae8748726c88fda83b4fbffa886baef2e60a8
gfoisy <gfoisy@u.rochester.edu>
2020-07-06 19:52:26 -0400

    semi-functional printing
    need to adjust how the extruder is handeled and such

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,25 @@ const extruder_bottom_editor=document.getElementById('extruder');
 let steps=0;
 let negpos=1;
 let height=7.5;
+let switcher=0;
 
 window.onwheel=function printGo(){
-  extruder_right.style= 'left: '+ -steps+'%;';
-  extruder_left.style= 'right: '+steps+'%;';
-  steps=steps+5*negpos;
-  if(steps>25){
+  steps=steps+2*negpos;
+  if(height>35){
+    return;
+  }else{  if(steps>7){
     negpos=-1;
-    height=height+5;
-    extruder_bottom_editor.style= 'bottom: '+ height+'%;';
-  }else if(steps<-25){
-    negpos=1;
-    height=height+5;
-    extruder_bottom_editor.style= 'bottom: '+ height+'%;';
+    height=height+2;
+    steps=steps+.6*negpos;
+    switcher=1;
+    }else if(steps<-7){
+    negpos=2;
+    height=height+2;
+    switcher=1;
+    steps=steps+.6*negpos;
     }
+    extruder_right.style= 'left: '+ -steps+'%;';
+    extruder_left.style= 'right: '+steps+'%;';
+    extruder_bottom_editor.style= 'bottom: '+ height+'%;';
+  }
 };
